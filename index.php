7caf13986d7066c87938f5171bfb92def869b4cf
DIGIKRASH <digitallkrash@gmail.com>
2023-01-16 17:27:37 +0600

    basic

--- index.php
+++ index.php
@@ -1,15 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP веб сайт</title>
+    <title>PHP | Мой первый сайт</title>
 
-    <link rel="stylesheet" href="./css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css" />
 </head>
+
 <body>
-    
+    <div class="container py-3">
+        <?php require "blocks/header.php" ?>
+
+        <h3 class="mb-5">Наши статьи</h3>
+
+        <div class="d-flex flex-wrap">
+            <?php for ($i = 1; $i <= 5; $i++): ?>
+                <div class="col">
+                    <div class="card mb-4 rounded-3 shadow-sm">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">Free</h4>
+                        </div>
+                        <div class="card-body">
+                            <img src="./img/<?php echo $i ?>.jpg" alt="">
+                            <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>10 users included</li>
+                                <li>2 GB of storage</li>
+                                <li>Email support</li>
+                                <li>Help center access</li>
+                            </ul>
+                            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+                        </div>
+                    </div>
+                </div>
+            <?php endfor ?>
+        </div>
+
+                
+        <?php require "blocks/footer.php" ?>
+    </div>
 </body>
+
 </html>
